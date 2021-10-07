dd28e552d04158c1cd36c36f939f9fdd8bfd0ffc
Sam10667 <samuel.animashaun@yahoo.com>
2021-10-07 18:00:58 +0100

    4

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,29 +2,24 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list
- * @h: head list
- * Return: size_t zise list
- **/
+ * print_list - function with one argument
+ * @h: const pointer type
+ *
+ * Description: prints all the elements of a list_t list
+ * Return: number of nodes
+ */
 size_t print_list(const list_t *h)
 {
-	unsigned int size = 0;
-	const list_t *listavarios = h;
+	unsigned int count = 0;
 
-	while (listavarios != NULL)
+	while (h != NULL)
 	{
-		if (listavarios->str == NULL)
-		{
-			printf("[%d] %s\n", 0, "(nil)");
-			listavarios = listavarios->next;
-			size++;
-		}
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 		else
-		{
-			printf("[%d] %s\n", listavarios->len, listavarios->str);
-			listavarios = listavarios->next;
-			size++;
-		}
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
 	}
-	return (size);
+	return (count);
 }
